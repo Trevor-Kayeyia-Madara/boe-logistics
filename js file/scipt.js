857316b2785ea51004a7ca6ec1561621b864941d
Michael ndolo <michaelndolo17142@gmail.com>
2024-01-30 20:57:18 +0300

    making dashboard well appealing

--- js file/scipt.js
+++ js file/scipt.js
@@ -1,7 +1,18 @@
 var addUser = document.getElementById('addUsers');
 var regFrame = document.getElementById('regFrame');
+var userList = document.getElementById('userList');
+var viewList = document.getElementById('viewList');
 
 addUser.onclick = function(){
     regFrame.style.visibility="visible";
+    userList.style.visibility="hidden";
     addUser.style.backgroundColor="#0d4683";
+    viewList.style.backgroundColor="#c7943f";
+}
+
+viewList.onclick = function(){
+    userList.style.visibility="visible";
+    regFrame.style.visibility="hidden";
+    viewList.style.backgroundColor="#0d4683";
+    addUser.style.backgroundColor="#c7943f";
 }
