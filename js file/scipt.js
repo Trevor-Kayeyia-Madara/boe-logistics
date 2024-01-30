1f8843d5234d4d31667dfa5fa6dec2cdab744017
Michael ndolo <michaelndolo17142@gmail.com>
2024-01-30 20:27:21 +0300

    functionality

---
+++ js file/scipt.js
@@ -0,0 +1,7 @@
+var addUser = document.getElementById('addUsers');
+var regFrame = document.getElementById('regFrame');
+
+addUser.onclick = function(){
+    regFrame.style.visibility="visible";
+    addUser.style.backgroundColor="#0d4683";
+}
