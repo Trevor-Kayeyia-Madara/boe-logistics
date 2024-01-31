c661e37c1f6e11555e2e09b7e27fc6631eb4a853
Michael ndolo <michaelndolo17142@gmail.com>
2024-01-31 07:44:15 +0300

    Update scipt.js

--- js file/scipt.js
+++ js file/scipt.js
@@ -2,17 +2,32 @@ var addUser = document.getElementById('addUsers');
 var regFrame = document.getElementById('regFrame');
 var userList = document.getElementById('userList');
 var viewList = document.getElementById('viewList');
+var dashboard = document.getElementById('dashboard');
+var visibleColor = "#0d4683";
+var hiddenColor = "#c7943f";
+
 
 addUser.onclick = function(){
     regFrame.style.visibility="visible";
     userList.style.visibility="hidden";
-    addUser.style.backgroundColor="#0d4683";
-    viewList.style.backgroundColor="#c7943f";
+    addUser.style.backgroundColor=visibleColor;
+    viewList.style.backgroundColor=hiddenColor;
+    dashboard.style.backgroundColor=hiddenColor;
 }
+
 
 viewList.onclick = function(){
     userList.style.visibility="visible";
     regFrame.style.visibility="hidden";
-    viewList.style.backgroundColor="#0d4683";
-    addUser.style.backgroundColor="#c7943f";
+    viewList.style.backgroundColor=visibleColor;
+    addUser.style.backgroundColor=hiddenColor;
+    dashboard.style.backgroundColor=hiddenColor;
+}
+
+dashboard.onclick = function(){
+    userList.style.visibility="hidden";
+    regFrame.style.visibility="hidden";
+    viewList.style.backgroundColor=hiddenColor;
+    addUser.style.backgroundColor=hiddenColor;
+    dashboard.style.backgroundColor=visibleColor;
 }
